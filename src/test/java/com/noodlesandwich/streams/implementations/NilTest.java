837999d64cc0646f099656f88269a7bfdc9f107a
Samir Talwar <samir@noodlesandwich.com>
2011-04-12 21:00:22 +0100

    Created tests for Cons.

--- src/test/java/com/noodlesandwich/streams/implementations/NilTest.java
+++ src/test/java/com/noodlesandwich/streams/implementations/NilTest.java
@@ -1,12 +1,11 @@
 package com.noodlesandwich.streams.implementations;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.junit.Test;
 
 import com.noodlesandwich.streams.EndOfStreamException;
 import com.noodlesandwich.streams.Stream;
+
+import static com.noodlesandwich.streams.matchers.NilMatcher.nil;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -25,20 +24,5 @@ public class NilTest {
     @Test(expected=EndOfStreamException.class) public void
     throws_an_exception_if_tail_is_called() {
         Stream.nil().tail();
-    }
-
-    private static <T> Matcher<Stream<T>> nil() {
-        return new TypeSafeDiagnosingMatcher<Stream<T>>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("nil");
-            }
-
-            @Override
-            protected boolean matchesSafely(Stream<T> item, Description mismatchDescription) {
-                mismatchDescription.appendText("the stream was not nil");
-                return item.isNil();
-            }
-        };
     }
 }
