bf2c5fe53007a28a4223416e8343ebfc7fa68c28
Samir Talwar <samir@noodlesandwich.com>
2011-04-14 00:13:41 +0100

    Removed logging from FilterTest.

--- src/test/java/com/noodlesandwich/streams/functions/FilterTest.java
+++ src/test/java/com/noodlesandwich/streams/functions/FilterTest.java
@@ -2,10 +2,10 @@ package com.noodlesandwich.streams.functions;
 
 import java.util.Arrays;
 
+import org.junit.Test;
+
 import com.google.common.base.Predicate;
 import com.noodlesandwich.streams.Stream;
-
-import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
@@ -14,9 +14,6 @@ public class FilterTest {
     @Test public void
     filtering_a_stream_with_a_predicate_returns_a_new_stream() {
         Stream<Integer> stream = Stream.wrap(Arrays.asList(1, 3, 2, 4, 7, 8, 9, 6, 5));
-        for (int i : stream.filter(isEven())) {
-            System.out.println(i);
-        }
         assertThat(stream.filter(isEven()), contains(2, 4, 8, 6));
     }
 
