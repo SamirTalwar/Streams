6e0f27c62f4f024ab1a483190e13eca7a90eaec1
Samir Talwar <samir@noodlesandwich.com>
2011-04-15 11:14:52 +0100

    Simplified the implementation of filter.

--- src/main/java/com/noodlesandwich/streams/functions/Filter.java
+++ src/main/java/com/noodlesandwich/streams/functions/Filter.java
@@ -1,7 +1,6 @@
 package com.noodlesandwich.streams.functions;
 
 import com.google.common.base.Predicate;
-import com.noodlesandwich.streams.EndOfStreamException;
 import com.noodlesandwich.streams.Stream;
 
 public final class Filter<T> extends Stream<T> {
@@ -15,25 +14,25 @@ public final class Filter<T> extends Stream<T> {
 
     @Override
     public boolean isNil() {
-        while (!stream.isNil() && !predicate.apply(stream.head())) {
-            stream = stream.tail();
-        }
-
+        filterNext();
         return stream.isNil();
     }
 
     @Override
     public T head() {
-        if (isNil()) {
-            throw new EndOfStreamException();
-        }
-
+        filterNext();
         return stream.head();
     }
 
     @Override
     public Stream<T> tail() {
-        head();
+        filterNext();
         return new Filter<T>(predicate, stream.tail());
+    }
+
+    private void filterNext() {
+        while (!stream.isNil() && !predicate.apply(stream.head())) {
+            stream = stream.tail();
+        }
     }
 }
--- src/test/java/com/noodlesandwich/streams/functions/FilterTest.java
+++ src/test/java/com/noodlesandwich/streams/functions/FilterTest.java
@@ -7,14 +7,21 @@ import org.junit.Test;
 import com.google.common.base.Predicate;
 import com.noodlesandwich.streams.Stream;
 
+import static com.noodlesandwich.streams.matchers.NilMatcher.nil;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
 
 public final class FilterTest {
     @Test public void
-    filtering_a_stream_with_a_predicate_returns_a_new_stream() {
+    filters_a_stream_with_a_predicate() {
         Stream<Integer> stream = Stream.wrap(Arrays.asList(1, 3, 2, 4, 7, 8, 9, 6, 5));
         assertThat(stream.filter(isEven()), contains(2, 4, 8, 6));
+    }
+
+    @Test public void
+    filters_nil_to_nil() {
+        assertThat(Stream.nil().filter(null), is(nil()));
     }
 
     private static Predicate<Integer> isEven() {
