51011c488e57431cc77578b61fc92a10229ffc05
Samir Talwar <samir@noodlesandwich.com>
2011-04-15 12:32:53 +0100

    Added extra tests for #toList and #toArray to ensure they don't change order or remove duplicates.

--- src/test/java/com/noodlesandwich/streams/functions/ToArrayTest.java
+++ src/test/java/com/noodlesandwich/streams/functions/ToArrayTest.java
@@ -22,4 +22,16 @@ public class ToArrayTest {
         Stream<Integer> stream = Stream.wrap(Arrays.asList(1, 2, 3));
         assertThat(stream.toArray(Integer.class), is(arrayContaining(1, 2, 3)));
     }
+
+    @Test public void
+    retains_order() {
+        Stream<Integer> stream = Stream.wrap(Arrays.asList(3, 2, 1));
+        assertThat(stream.toArray(Integer.class), is(arrayContaining(3, 2, 1)));
+    }
+
+    @Test public void
+    retains_duplicates() {
+        Stream<Integer> stream = Stream.wrap(Arrays.asList(2, 3, 2, 1));
+        assertThat(stream.toArray(Integer.class), is(arrayContaining(2, 3, 2, 1)));
+    }
 }
--- src/test/java/com/noodlesandwich/streams/functions/ToListTest.java
+++ src/test/java/com/noodlesandwich/streams/functions/ToListTest.java
@@ -22,4 +22,16 @@ public class ToListTest {
         Stream<Integer> stream = Stream.wrap(Arrays.asList(1, 2, 3));
         assertThat(stream.toList(), contains(1, 2, 3));
     }
+
+    @Test public void
+    retains_order() {
+        Stream<Integer> stream = Stream.wrap(Arrays.asList(3, 2, 1));
+        assertThat(stream.toList(), contains(3, 2, 1));
+    }
+
+    @Test public void
+    retains_duplicates() {
+        Stream<Integer> stream = Stream.wrap(Arrays.asList(2, 3, 2, 1));
+        assertThat(stream.toList(), contains(2, 3, 2, 1));
+    }
 }
