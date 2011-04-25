6f38548d9c9ee9f5e670138b4e448e0ca368999a
Samir Talwar <samir@noodlesandwich.com>
2011-04-25 01:41:09 +0100

    Added a test to ensure #sort is a stable sort.

--- src/test/java/com/noodlesandwich/streams/functions/SortTest.java
+++ src/test/java/com/noodlesandwich/streams/functions/SortTest.java
@@ -1,5 +1,7 @@
 package com.noodlesandwich.streams.functions;
 
+import java.util.Comparator;
+
 import org.junit.Test;
 
 import com.google.common.collect.Ordering;
@@ -27,5 +29,20 @@ public class SortTest {
     sorts_objects_according_to_a_comparator() {
         Stream<Integer> stream = Stream.of(7, 5, 8, 1, 2, 9, 3, 6, 4);
         assertThat(stream.sort(Ordering.natural()), contains(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    }
+
+    @Test public void
+    is_stable() {
+        Stream<Integer> stream = Stream.of(7, 5, 8, 1, 2, 9, 3, 6, 4);
+        assertThat(stream.sort(evensAreOdds()), contains(1, 2, 3, 5, 4, 7, 6, 8, 9));
+    }
+
+    private static Comparator<? super Integer> evensAreOdds() {
+        return new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return Integer.valueOf(a - a % 2).compareTo(b - b % 2);
+            }
+        };
     }
 }
