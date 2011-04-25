a11a65b8f58007363d8cd446875c6d479d67fb1e
Samir Talwar <samir@noodlesandwich.com>
2011-04-25 23:36:41 +0100

    Added JavaDocs for the Stream class and its constructors.

--- src/main/java/com/noodlesandwich/streams/Stream.java
+++ src/main/java/com/noodlesandwich/streams/Stream.java
@@ -36,19 +36,66 @@ import com.noodlesandwich.streams.implementations.IteratorWrapper;
 import com.noodlesandwich.streams.implementations.Nil;
 import com.noodlesandwich.streams.iterators.StreamIterator;
 
+/**
+ * <p>A Stream is an immutable structure similar to a linked list in design. Once created, the stream cannot be altered,
+ * and any operation performed on it will create a new stream. The only way to access the data in a stream is to
+ * iterate over it, either by using a <em>for each</em> loop, or by retrieving the {@link #iterator() iterator}
+ * directly, or by using the {@link #head()} or {@link #tail()} methods to access the head or the tail of the stream.
+ * In addition, the majority of the operations provided are lazy &mdash; that is, they will not be evaluated until the
+ * stream is iterated over or the values within the stream are accessed.</p>
+ *
+ * <p>Streams are designed to be used with immutable data types. As the stream itself cannot be manipulated, using only
+ * immutable types ensures no other code will alter the underlying values within the stream once it has been created,
+ * ensuring thread safety.</p>
+ *
+ * <p>Streams can be created in many ways:</p>
+ *
+ * <ul>
+ *   <li>{@link Stream#nil Stream.nil()} returns an empty stream &mdash; one with no elements.</li>
+ *   <li>{@link Stream#cons Stream.cons(T head, Stream&lt;T&gt; tail)} creates a new stream by prepending an element to
+ *       an existing stream.</li>
+ *   <li>{@link Stream#of Stream.of(a, b, c)} is a shortcut for <code>cons(a, cons(b, cons(c, nil())))</code>.</li>
+ *   <li>{@link Stream#clone Stream.clone(Iterable&lt;T&gt;)} will clone an iterable by iterating over it.</li>
+ *   <li>{@link Stream#wrap(Iterator) Stream.wrap(Iterator&lt;T&gt;)} wraps a mutable, one-use iterator in an immutable,
+ *       many-use stream.</li>
+ *   <li>{@link Stream#generate Stream.generate(Function&lt;T, T&gt; function, T start)} generates an infinite stream
+ *       using a start value and a function that is applied to each successive result to produce each value in the
+ *       stream.<li>
+ *   <li>{@link Stream#repeat(Object) Stream.repeat(T value)} creates an infinite stream by repeating the same
+ *       value.</li>
+ * </ul>
+ */
 public abstract class Stream<T> implements Iterable<T> {
-    public static <T> Stream<T> cons(T head, Stream<T> tail) {
-        return new Cons<T>(head, tail);
-    }
-
+    /**
+     * Creates an empty stream. Attempts to call {@link #head()} or {@link #tail()} on this stream will throw an
+     * {@link EndOfStreamException}
+     */
     public static <T> Stream<T> nil() {
         return new Nil<T>();
     }
 
+    /**
+     * <p>Creates a new stream by prepending <code>head</code> to <code>tail</code>. Can be nested to build up a large
+     * stream. It is expected that the final value in the chain will be {@link #nil()}.</p>
+     *
+     * <strong>Example:</strong>
+     *   <code>cons(a, cons(b, cons(c, nil())))</code> will return a stream that when iterated over, will yield
+     *   <code>a</code>, then <code>b</code>, then <code>c</code>.
+     */
+    public static <T> Stream<T> cons(T head, Stream<T> tail) {
+        return new Cons<T>(head, tail);
+    }
+
+    /**
+     * Creates a new stream over the values provided. This is shorthand for nested calls to {@link #cons}.
+     */
     public static <T> Stream<T> of(T... items) {
         return clone(Iterators.forArray(items));
     }
 
+    /**
+     * Clones an iterable by iterating over it and building up a stream.
+     */
     public static <T> Stream<T> clone(Iterable<T> iterable) {
         return clone(iterable.iterator());
     }
@@ -61,18 +108,33 @@ public abstract class Stream<T> implements Iterable<T> {
         return cons(iterator.next(), clone(iterator));
     }
 
+    /**
+     * Wraps an iterable. Delegates to {@link #wrap(Iterator) wrap(Iterator&lt;T&gt;)}.
+     */
     public static <T> Stream<T> wrap(Iterable<T> iterable) {
         return wrap(iterable.iterator());
     }
 
+    /**
+     * Wraps an iterator with a stream that retains values as they are retrieved, ensuring the stream can be iterated
+     * over multiple times.
+     */
     public static <T> Stream<T> wrap(Iterator<T> iterator) {
         return new IteratorWrapper<T>(iterator);
     }
 
+    /**
+     * Generates a stream using a function that is applied to each successive element, starting with the start value
+     * provided. This stream will normally be infinite, but the iterating function can terminate it by throwing an
+     * {@link EndOfStreamException}.
+     */
     public static <T> Stream<T> generate(Function<? super T, ? extends T> iteratingFunction, T start) {
         return new Generator<T>(iteratingFunction, start);
     }
 
+    /**
+     * Generates an infinite stream by repeating the value provided.
+     */
     public static <T> Stream<T> repeat(T value) {
         return generate(Functions.<T>identity(), value);
     }
